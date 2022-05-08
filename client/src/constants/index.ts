329c6080dcec1fead77c16ca4a3511c4ee7a7a83
Tadjh <tadjh.brooks@gmail.com>
2022-05-08 15:59:27 -0700

    fix: reuse variable

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,4 +1,4 @@
 export { models } from "./models";
 
 export const CURRENT_RESOURCE_NAME = GetCurrentResourceName();
-export const COMMAND_PED = "immersive-ped";
+export const COMMAND_PED = CURRENT_RESOURCE_NAME;
