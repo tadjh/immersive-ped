5e14f2f0a539f8458b5f04705c6b5a12f6813f27
Tadjh <tadjh.brooks@gmail.com>
2022-05-08 14:11:39 -0700

    fix: remove comments

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,7 +1,4 @@
 export { models } from "./models";
 
-// Evironment
 export const CURRENT_RESOURCE_NAME = GetCurrentResourceName();
-
-// Commands
 export const COMMAND_PED = "immersive-ped";
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,8 +1,6 @@
 import { COMMAND_PED } from "./constants";
 import { ped } from "./features";
 
-// Commands
 RegisterCommand(COMMAND_PED, ped, false);
 
-// Exports
 globalThis.exports("ped", ped);
